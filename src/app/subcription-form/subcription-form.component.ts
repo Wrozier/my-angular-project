bb12ed30504059bd4e5f923c18e3545d3a42f5d0
William Rozier <GQ@Wills-MacBook-Air.local>
2023-07-16 19:06:26 -0400

    single-post commit

---
+++ src/app/subcription-form/subcription-form.component.ts
@@ -0,0 +1,10 @@
+import { Component } from '@angular/core';
+
+@Component({
+  selector: 'app-subcription-form',
+  templateUrl: './subcription-form.component.html',
+  styleUrls: ['./subcription-form.component.css']
+})
+export class SubcriptionFormComponent {
+
+}
